62b338c26074b159f7de52b0850490eeef58824d
赵倩倩 <qianqian.zhao@xbongbong.com>
2020-04-04 17:20:51 +0800

    feat: 抽饭程序开发

--- src/views/select-meal.vue
+++ src/views/select-meal.vue
@@ -2,12 +2,45 @@
  * @Author: qianqian.zhao
  * @Date: 2020-04-03 16:44:04
  * @LastEditors: qianqian.zhao
- * @LastEditTime: 2020-04-03 16:44:19
+ * @LastEditTime: 2020-04-04 17:09:49
  * @Description: 选择菜单
  -->
 <template>
-  <div class="selectMeal">
+  <div class="select-meal">
+    喵呜，菜单，啦啦啦
+    <br>
+    <el-checkbox-group v-model="todayMenu">
+      <el-checkbox v-for="item in menuList" :key="item" :label="item" :value="item">
+        {{item}}
+      </el-checkbox>
+    </el-checkbox-group>
 
+    <div>
+      <el-button @click="deletePersonFood('yuxi')">喵呜，昱晰不吃</el-button>
+      以下食物将不会参与抽签:
+      <template v-for="item of yuxi">
+        <span :key="item">{{item}},</span>
+      </template>
+      <br>
+      <el-button @click="deletePersonFood('laodu')">喵呜，老杜不吃</el-button>
+      以下食物将不会参与抽签:
+      <template v-for="item of laodu">
+        <span :key="item">{{item}},</span>
+      </template>
+      <br>
+      <el-button @click="deletePersonFood('biyun')">喵呜，碧云不吃</el-button>
+      以下食物将不会参与抽签:
+      <template v-for="item of biyun">
+        <span :key="item">{{item}},</span>
+      </template>
+    </div>
+
+    <el-button @click="checkMenu">点击抽取吃啥</el-button>
+
+
+    <br>
+    <div>我想让你今天吃{{todayselected}}</div>
+    <!-- {{menuArr}} -->
   </div>
 </template>
 
@@ -29,6 +62,110 @@ export default {
 
   data () {
     return {
+      menuList: ['米线', '酸辣粉', '麻辣烫', '陕西老碗', '淮南牛肉面', '寿司', '脱骨鸡',
+        '小笼包', '德克士', '麦当劳', '食其家', '粥饼世家', '冷面', '花甲', '牛肉粉', '黄焖鸡', '鸡丝凉面'],
+      yuxi: ['老包子', '小笼包', '德克士', '麦当劳', '寿司'],
+      laodu: ['凉皮', '衢饭儿'],
+      biyun: [],
+      copyMenuList: [
+        {
+          name: '米线',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '酸辣粉',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '麻辣烫',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '凉皮',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '淮南牛肉面',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '寿司',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '脱骨鸡',
+          cold: false,
+          spicy: false,
+        },
+        // {
+        //   name: '屋力家',
+        //   cold: false,
+        //   spicy: false,
+        // },
+        {
+          name: '老包子',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '小笼包',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '德克士',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '麦当劳',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '食其家',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '粥饼世家',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '香锅',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '冷面',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '衢饭儿',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '花甲',
+          cold: false,
+          spicy: false,
+        },
+        {
+          name: '牛肉粉',
+          cold: false,
+          spicy: false,
+        },
+      ],
+      todayMenu: [],
+      todayselected: '',
     }
   },
 
@@ -39,9 +176,19 @@ export default {
   },
 
   mounted () {
+    console.log('实例挂载完成');
+    this.todayMenu = this.menuList;
   },
 
   methods: {
+    checkMenu() {
+      const menuLength = this.todayMenu.length;
+      const todayselected = Math.floor(Math.random() * menuLength);
+      this.todayselected = this.todayMenu[todayselected];
+    },
+    deletePersonFood(person) {
+      this.todayMenu = this.todayMenu.filter(item => !this[person].includes(item));
+    },
   }
 }
 </script>
