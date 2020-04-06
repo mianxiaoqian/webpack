622b6da4d18f9aaeca6bee6578b788486104f9e6
赵倩倩 <qianqian.zhao@xbongbong.com>
2020-04-06 22:53:37 +0800

    feat: 样式架构

--- src/app.vue
+++ src/app.vue
@@ -2,48 +2,27 @@
  * @Author: qianqian.zhao
  * @Date: 2020-03-25 18:50:33
  * @LastEditors: qianqian.zhao
- * @LastEditTime: 2020-04-04 16:36:29
+ * @LastEditTime: 2020-04-04 17:27:12
  * @Description: 单页面首页组件
  -->
 
 <template>
   <div class="miao">
-    这是项目的根目录文件app
-    <!-- <home></home> -->
+    <!-- 这是项目的根目录文件app -->
     <!-- 路由匹配的组件渲染到这里 -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import Home from './views/home'
   export default {
     name: 'app',
 
-    components: {
-      Home
-    },
-
-    filters: {
-    },
-
-    mixins: [
-    ],
-
-    props: {
-    },
-
     data () {
       return {
-        msg: 'this is my app!'
       }
     },
 
-    computed: {
-    },
-
-    watch: {
-    },
     beforeCreate () {
       console.log('App实例创建之前,什么都访问不到')
       console.log(this, this.$el, this.$option, this.$data)
@@ -61,9 +40,6 @@
       debugger
       console.log('App实例挂载完成，真实数据进行占位填充')
       console.log(this, this.$el, this.$option, this.$data)
-    },
-
-    methods: {
     }
   }
 </script>
